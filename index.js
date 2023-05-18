2bdd2317e5699d6edcb9bf20d21b230b5e7195ce
PktBst <80565240+PktBst@users.noreply.github.com>
2023-05-18 20:38:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  socket.on('update', (text) => {
+    io.emit('update', text);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
